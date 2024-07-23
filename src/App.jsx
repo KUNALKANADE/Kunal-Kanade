72b9d410f8de076c6011cd4ccfc29747bbfd51bf
KUNALKANADE <atkunal23000@gmail.com>
2024-07-23 20:03:48 +0530

    Integrate @vercel/analytics and update application structure, add new assets

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import Enquiry from "./component/Enquiry";
 
 
 
+
+
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { inject } from '@vercel/analytics';
+import './index.css';
 
+inject();
+
+// Create a root and render the App
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
